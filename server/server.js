479500b9dfdf12eddda3b161bca454f2d044d51c
imrituraj <ritur.dev@gmail.com>
2025-05-01 11:34:30 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const authRoutes = require("./routes/auth");
+const marketRoutes = require("./routes/market");
+
+app.use("/api/auth", authRoutes);
+app.use("/api/market", marketRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error(err));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
